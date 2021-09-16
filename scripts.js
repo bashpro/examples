1bc4b7d7f34ab272ad61adcce073d72c7057a1b3
Bash <bashash10@gmail.com>
2021-09-17 00:14:34 +0100

    Update scripts.js
    more changes

--- scripts.js
+++ scripts.js
@@ -1,31 +1,55 @@
-let version = "0.1.1";
+let version = "0.1.4";
 
-let currentVer = "0.1.4"
+let currentVer = "0.2.3"
+
 
 let verChecker = (i, o) => {
 	let v = i.split(".");
   let current = o.split(".");
-  let fail;
+	let fail,
+  		pass,
+      not,
+      p = 0
+
 
   for(let e = 0; e < 3; e++){
 
   	if(v[e] > current[e]){
-    	console.log( v[e] + " is less than current");
-    }
-    if(v[e] === current[e]){
+    	console.log( v[e] + " is more than current");
+      not = true
+
+
+    }else if(v[e] === current[e]){
+
     	console.log( v[e] + " pass");
-    }
+      p++
 
-    if(v[e] < current[e]){
+      if(p === 3){
+      	pass = true
+      }
+
+
+    }else if(v[e] < current[e]){
     	console.log( v[e] + " is lovwer than current");
-      let fail = true;
+      fail = true;
     }
-
 
   }
 
-  if(fail){
-    console.log("unfortunately you are not on the current version " + currentVer)
+  if(fail === true){
+  	console.log("unfortunately, "+version+" needs updating to the current version " + currentVer)
+  }else{
+  	//console.log("you are not on the current version " + currentVer)
+  }
+
+  if(pass){
+  	console.log("you are running the latest version " + currentVer)
+  }
+
+  if(not === true){
+    console.log("You are running " + version + " which is not supported")
+  }else{
+  	//console.log("you are on the current version " + currentVer)
   }
 
 }
